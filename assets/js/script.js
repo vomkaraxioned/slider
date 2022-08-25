ee6089bf51600d1e06228e40ccd6f17b4aaaa0b7
vomkaraxioned <omkarv@axioned.com>
2022-08-25 16:18:31 +0530

    css changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,18 @@ const slides = document.getElementsByClassName("slide");
 const prev = document.querySelector(".prev");
 const nxt = document.querySelector(".next")
 const buttons = document.querySelectorAll(".img");
-let prevIndex;
 let currentIndex;
-let anime;
 buttons.forEach((btn, index) => {
-    btn.index = index;
     btn.addEventListener('click', () => {
-        currentIndex = btn.index;
+        currentIndex = index;
         slide();
     });
 });
-
 prev.addEventListener('click', () => {
     if (currentIndex > 0)
         currentIndex -= 1;
     else
         currentIndex = slides.length - 1;
-    anime = "slide-right";
     slide();
 });
 
@@ -31,14 +26,12 @@ nxt.addEventListener('click', () => {
         currentIndex += 1;
     else
         currentIndex = 0;
-    anime = "slide-left";
     slide();
 });
 
 function slide() {
     let active = document.querySelector(".active");
     let activeBtn = document.querySelector(".btn-active");
-    active.style.animation = anime;
     active.classList.remove('active');
     activeBtn.classList.remove('btn-active');
     slides[currentIndex].classList.add('active');
