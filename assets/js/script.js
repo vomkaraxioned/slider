220611c8932d813dd509a191118dd9d127137910
vomkaraxioned <omkarv@axioned.com>
2022-08-25 18:47:23 +0530

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,8 @@ const slides = document.getElementsByClassName("slide");
 const prev = document.querySelector(".prev");
 const nxt = document.querySelector(".next")
 const buttons = document.querySelectorAll(".img");
-let currentIndex;
+let prevIndex = 0;
+let currentIndex = 0;
 buttons.forEach((btn, index) => {
     btn.addEventListener('click', () => {
         currentIndex = index;
@@ -34,6 +35,21 @@ function slide() {
     let activeBtn = document.querySelector(".btn-active");
     active.classList.remove('active');
     activeBtn.classList.remove('btn-active');
+    alert(currentIndex);
+    if (currentIndex > prevIndex) {
+        active.classList.add("expand-next");
+        setTimeout(() => {
+            active.classList.remove("expand-next");
+        }, 1000);
+
+    } else {
+        active.classList.add("expand-prev");
+        setTimeout(() => {
+            active.classList.remove("expand-prev");
+        }, 1000);
+
+    }
     slides[currentIndex].classList.add('active');
     buttons[currentIndex].classList.add('btn-active');
+    prevIndex = currentIndex;
 }
