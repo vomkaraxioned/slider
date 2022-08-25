158bed6aeca99eb3554ffbe611f384c00890152d
vomkaraxioned <omkarv@axioned.com>
2022-08-25 13:46:43 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+/* Author: 
+
+*/
+
+const slides = document.getElementsByClassName("slide");
+const prev = document.querySelector(".prev");
+const nxt = document.querySelector(".next")
+const buttons = document.querySelectorAll(".img");
+let currentIndex;
+
+buttons.forEach((btn, index) => {
+    btn.index = index;
+    btn.addEventListener('click', () => {
+        currentIndex = btn.index;
+        slide();
+    });
+});
+
+prev.addEventListener('click', () => {
+    if (currentIndex > 0)
+        currentIndex -= 1;
+    else
+        currentIndex = slides.length - 1;
+    slide();
+});
+
+nxt.addEventListener('click', () => {
+    if (currentIndex < slides.length - 1)
+        currentIndex += 1;
+    else
+        currentIndex = 0;
+    slide();
+});
+
+function slide() {
+    let active = document.querySelector(".active");
+    let activeBtn = document.querySelector(".btn-active");
+    active.classList.remove('active');
+    activeBtn.classList.remove('btn-active');
+    slides[currentIndex].classList.add('active');
+    buttons[currentIndex].classList.add('btn-active');
+}
